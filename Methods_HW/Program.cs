f7393e48af1984c0185be9bd5aab9d6c6d505d95
Daria Shadrina <darinashadrina@yandex.ru>
2022-03-17 15:23:45 +0300

    Create project for HW1 number 1

--- Methods_HW/Program.cs
+++ Methods_HW/Program.cs
@@ -6,7 +6,25 @@ namespace Methods_HW
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Пользователь вводит 2 числа (A и B). Выведите в консоль результат деления A на B и остаток от деления.
+
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Divide(A, B));
+            Console.WriteLine(CalculateTheRemainder(A, B));
+        }
+
+        static double Divide(double divisible, double divisor)
+        {
+            double result = divisible / divisor;
+            return result;
+        }
+
+        static double CalculateTheRemainder(double divisible, double divisor)
+        {
+            double remainder = divisible % divisor;
+            return remainder;
         }
     }
 }
